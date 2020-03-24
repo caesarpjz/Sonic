48e8fa81471a03815bf0eca2c9ad5a5ab8cf01db
Min S <mindysoh@gmail.com>
2020-03-24 08:13:52 +0800

    add routing

---
+++ frontend-customer/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { HomeComponent } from './home/home.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule],
+  declarations: []
+})
+export class AppRoutingModule { }
--- frontend-customer/src/app/app.module.ts
+++ frontend-customer/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { CategoryComponent } from './category/category.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoryComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
