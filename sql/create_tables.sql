99817ba282b1a6ad918c44c60a8417c925c2e4dd
weigenie <ieeweigen@hotmail.com>
2020-03-28 14:44:49 +0800

    tables init

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,2 +1,88 @@
 -- CREATE DATABASE Sonic;
 
+
+-- USER
+CREATE TABLE Users (
+
+);
+
+-- RESTAURANTS
+CREATE TABLE Restaurants (
+    
+);
+
+CREATE TABLE Menus(
+    
+);
+
+CREATE TABLE Food_Items(
+    
+);
+
+CREATE TABLE Promotions (
+    
+);
+
+-- CUSTOMERS
+CREATE TABLE Customers (
+
+);
+
+CREATE TABLE Reviews (
+    
+);
+
+-- ORDERS
+CREATE TABLE Orders (
+    
+);
+
+CREATE TABLE Order_Contains_Food (
+    
+);
+
+CREATE TABLE Customer_Rates_Delivery (
+    
+);
+
+-- RIDERS 
+CREATE TABLE Riders (
+    
+);
+
+CREATE TABLE Rider_Delivers_Orders (
+    
+);
+
+CREATE TABLE Full_Time (
+    
+);
+
+CREATE TABLE Part_Time (
+    
+);
+
+CREATE TABLE MWS (
+    
+);
+
+CREATE TABLE WWS (
+    
+);
+
+CREATE TABLE Shifts (
+    
+);
+
+-- FDS MANAGER
+CREATE TABLE FDS_Managers (
+    
+);
+
+CREATE TABLE Reports (
+    
+);
+
+CREATE TABLE Restaurant_Staff (
+    
+);
