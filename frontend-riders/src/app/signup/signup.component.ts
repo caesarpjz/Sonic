e9648fe2bf3a041150b6a47a766ae09ed7869da2
Min S <mindysoh@gmail.com>
2020-03-31 18:09:11 +0800

    sign up form

--- frontend-riders/src/app/signup/signup.component.ts
+++ frontend-riders/src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  signupForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
+    this.initSignupForm();
+  }
 
   ngOnInit() {
+  }
+
+  initSignupForm() {
+    this.signupForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(/^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/)
+        ])
+      ],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required]
+    }, { validator: this.checkPasswords });
+  }
+
+  checkPasswords(group: FormGroup) {
+    let pass = group.get('password').value;
+    let confirmPass = group.get('confirmPassword').value;
+
+    return pass === confirmPass ? null : { notSame: true };
+  }
+
+  signUp() {
+    console.log(this.signupForm);
   }
 
 }
