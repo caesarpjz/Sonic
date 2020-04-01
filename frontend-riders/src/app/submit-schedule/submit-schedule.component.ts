3e68e86252ff79af84fb2d3cc1f0291b89ac2efd
Min S <mindysoh@gmail.com>
2020-04-01 08:07:10 +0800

    bump from angular 5 to 9

--- frontend-riders/src/app/submit-schedule/submit-schedule.component.ts
+++ frontend-riders/src/app/submit-schedule/submit-schedule.component.ts
@@ -1,10 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { SelectItem } from 'primeng/api';
-
-interface City {
-  name: string;
-  code: string;
-}
 
 @Component({
   selector: 'app-submit-schedule',
@@ -12,36 +6,9 @@ interface City {
   styleUrls: ['./submit-schedule.component.css']
 })
 export class SubmitScheduleComponent implements OnInit {
-  cities1: SelectItem[];
-
-  cities2: City[];
-
-  selectedCity1: City;
-
-  selectedCity2: City;
-
   constructor() {
-    //SelectItem API with label-value pairs
-    this.cities1 = [
-      { label: 'Select City', value: null },
-      { label: 'New York', value: { id: 1, name: 'New York', code: 'NY' } },
-      { label: 'Rome', value: { id: 2, name: 'Rome', code: 'RM' } },
-      { label: 'London', value: { id: 3, name: 'London', code: 'LDN' } },
-      { label: 'Istanbul', value: { id: 4, name: 'Istanbul', code: 'IST' } },
-      { label: 'Paris', value: { id: 5, name: 'Paris', code: 'PRS' } }
-    ];
-
-    //An array of cities
-    this.cities2 = [
-      { name: 'New York', code: 'NY' },
-      { name: 'Rome', code: 'RM' },
-      { name: 'London', code: 'LDN' },
-      { name: 'Istanbul', code: 'IST' },
-      { name: 'Paris', code: 'PRS' }
-    ];
   }
 
   ngOnInit() {
   }
-
 }
