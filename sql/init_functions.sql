74e9dbfcdd0a80b3109dd99531a1625d12dd1a8f
caesarpjz <caesarpjz@gmail.com>
2020-05-06 17:16:09 +0800

    changes

--- sql/init_functions.sql
+++ sql/init_functions.sql
@@ -336,11 +336,10 @@ CREATE OR REPLACE FUNCTION addFoodItem(
     name VARCHAR, 
     price FLOAT, 
     menu_id INTEGER,
-    category VARCHAR, 
-    availability BOOLEAN)
+    category VARCHAR)
 RETURNS void AS $$
     INSERT INTO Food_Items
-    VALUES (DEFAULT, quantity, daily_limit, name, price, menu_id, category, availability);
+    VALUES (DEFAULT, quantity, daily_limit, name, price, menu_id, category, TRUE);
 $$ LANGUAGE SQL;
 
 CREATE OR REPLACE FUNCTION addRestaurantPromotion(start_date DATE, end_date DATE, discount_desc TEXT, 
