1d213d2b2584a00e080da4ead298543e118af5a3
Min S <mindysoh@gmail.com>
2020-05-07 19:31:27 +0800

    riders: view schedule && add momentjs

--- frontend-riders/src/app/view-schedule/view-schedule.component.ts
+++ frontend-riders/src/app/view-schedule/view-schedule.component.ts
@@ -1,7 +1,10 @@
+import { ScheduleService } from './../services/schedule.service';
 import { Component, OnInit } from '@angular/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
+
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-view-schedule',
@@ -9,43 +12,41 @@ import interactionPlugin from '@fullcalendar/interaction';
   styleUrls: ['./view-schedule.component.css']
 })
 export class ViewScheduleComponent implements OnInit {
-  events: any[];
+  events = [];
 
   options: any;
 
-  constructor() { }
+  constructor(private scheduleService: ScheduleService) { }
 
   ngOnInit(): void {
-    // this.eventService.getEvents().then(events => { this.events = events; });
-
-    this.events = [
-        {
-          "id": 1,
-          "title": "All Day Event",
-          "start": "2017-02-01"
-        },
-        {
-          "id": 2,
-          "title": "Long Event",
-          "start": "2017-02-07",
-          "end": "2017-02-10"
-        },
-        {
-          "id": 3,
-          "title": "Repeating Event",
-          "start": "2017-02-09T16:00:00"
-        }
-      ];
+    this.getSchedule();
 
     this.options = {
       plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
-      defaultDate: '2017-02-01',
+      defaultDate: '2020-05-08',
       header: {
         left: 'prev,next',
         center: 'title',
         right: 'dayGridMonth,timeGridWeek,timeGridDay'
       }
     };
+  }
+
+  getSchedule() {
+    this.scheduleService.getSchedule().subscribe((res) => {
+      const schedule = res;
+      // transform data
+      schedule.map((shift) => {
+        let transformedShift = {
+          'id': shift.shift_id,
+          'title': 'Shift',
+          'start': moment.parseZone(shift.start_time).local().format(),
+          'end': moment.parseZone(shift.end_time).local().format()
+        }
+
+        this.events.push(transformedShift);
+      });
+    });
   }
 
 }
